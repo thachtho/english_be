aed71e6e90751df6c1efe4574bf9f48f6a797a17
thobui14ig <buithanhtho14ig@gmail.com>
2024-04-22 23:40:46 +0700

    ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,8 +28,8 @@ import { LessonModule } from './lesson/lesson.module';
       useFactory() {
         return {
           type: 'mysql',
-          host: 'localhost',
-          port: 3306,
+          host: '103.214.10.223',
+          port: 3307,
           username: 'root',
           password: '111111',
           database: 'english',
